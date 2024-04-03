fa4831a358635a64f2826a6b9c9727e4d154b623
Ihor Serba <ihor.serba@gmail.com>
2024-04-03 14:48:53 +0300

    Reduce diff

--- pkg/v2/crud/sort_by.go
+++ pkg/v2/crud/sort_by.go
@@ -2,7 +2,6 @@ package crud
 
 import (
 	"fmt"
-
 	"github.com/imulab/go-scim/pkg/v2/crud/expr"
 	"github.com/imulab/go-scim/pkg/v2/prop"
 	"github.com/imulab/go-scim/pkg/v2/spec"
@@ -12,46 +11,39 @@ import (
 // sorted. 'by' is the compiled sortBy path. Depending on the sortBy parameter, the actual sort target varies in several cases.
 //
 // First, if the sortBy parameter refers to a simple type inside a singular complex type, it will be the sort target. For instance,
-//
 //	sortBy=name.familyName
-//
 // Given the following resource snippet:
-//
-//	{
-//		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
-//		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
-//		"userName": "imulab",
-//		"name": {
-//			"familyName": "Qiu",
-//			"givenName": "David"
-//		}
-//	}
+// 	{
+// 		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+// 		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
+// 		"userName": "imulab",
+// 		"name": {
+// 			"familyName": "Qiu",
+// 			"givenName": "David"
+// 		}
+// 	}
 //
 // The value "Qiu" will be used to sort the resource. This also applies to any top level singular simple types, as this
 // project treats top-level container as a virtual complex property as well.
 //
 // Second, if the sortBy parameter refers to a simple type inside a multiValued complex type, the sort target will be
 // the value whose accompanying primary attribute was set to 'true', or the first value. For instance,
-//
 //	sortBy=emails.value
-//
 // Given the following resource snippet:
-//
-//	{
-//		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
-//		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
-//		"userName": "imulab",
-//		"emails": [
-//			{
-//				"value": "foo@bar.com"
-//			},
-//			{
-//				"value": "bar@foo.com",
-//				"primary": true
-//			}
-//		]
-//	}
-//
+// 	{
+// 		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+// 		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
+// 		"userName": "imulab",
+// 		"emails": [
+// 			{
+// 				"value": "foo@bar.com"
+// 			},
+// 			{
+// 				"value": "bar@foo.com",
+// 				"primary": true
+// 			}
+// 		]
+// 	}
 // The value "bar@foo.com" will be used to sort the resource, because its accompany primary attribute is true.
 //
 // However, if none of the primary attribute was set to true, like:
