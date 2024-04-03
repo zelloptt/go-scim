194e449f4c751d17bcddefdcef1ece6ce76461d6
Ihor Serba <ihor.serba@gmail.com>
2024-04-03 14:51:18 +0300

    Reduce diff

--- pkg/v2/crud/sort_by.go
+++ pkg/v2/crud/sort_by.go
@@ -47,41 +47,36 @@ import (
 // The value "bar@foo.com" will be used to sort the resource, because its accompany primary attribute is true.
 //
 // However, if none of the primary attribute was set to true, like:
-//
-//	{
-//		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
-//		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
-//		"userName": "imulab",
-//		"emails": [
-//			{
-//				"value": "foo@bar.com"
-//			},
-//			{
-//				"value": "bar@foo.com"
-//			}
-//		]
-//	}
-//
+// 	{
+// 		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+// 		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
+// 		"userName": "imulab",
+// 		"emails": [
+// 			{
+// 				"value": "foo@bar.com"
+// 			},
+// 			{
+// 				"value": "bar@foo.com"
+// 			}
+// 		]
+// 	}
 // In this case, "foo@bar.com" will be used to sort the resource, because it is the first value.
 //
 // Third, if the sortBy parameter refers to multiValued simple type, the sort target will be its first element. For instance,
-//
 //	sortBy=schemas
-//
 // Given the following resource snippet:
-//
-//		{
-//			"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User", "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"],
-//			"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
-//			"userName": "imulab",
-//			"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
-//	 			"employeeNumber": "11250"
-//			}
-//		}
-//
+// 	{
+// 		"schemas": ["urn:ietf:params:scim:schemas:core:2.0:User", "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"],
+// 		"id": "FA33AA7E-99DB-4446-A2CD-050133B442DA",
+// 		"userName": "imulab",
+// 		"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+//  			"employeeNumber": "11250"
+// 		}
+// 	}
 // The value "urn:ietf:params:scim:schemas:core:2.0:User" will be used to sort the resource because it is the first element.
 //
 // Other sortBy parameters that does not fall into the above three categories are considered to be invalid.
+//
 func SeekSortTarget(resource *prop.Resource, by *expr.Expression) (prop.Property, error) {
 	if by.ContainsFilter() {
 		return nil, fmt.Errorf("%w: sortBy attribute cannot contain filter", spec.ErrInvalidPath)
